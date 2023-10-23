624ea8cf1336acb850a5c1b1895349647944ebc8
jcright73 <jcright73@gmail.com>
2023-10-23 14:26:33 -0400

    Adds my changes

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const appendCat = [...cats, name];
+}
+
+function prependCat(name){
+    const appendCat = [name, ...cats];
+}
+
+function removeLastCat(){
+    const removeLastCat = cats.slice(-1);
+}
+
+function removeFirstCat(){
+    const removeFirstCat = cats.slice(1);
+}
